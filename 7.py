cf23f169441fa102c317aa24d2977c039206ba03
artur_boltyansky <artur.boltiansky@assuredallies.com>
2022-08-11 18:31:20 +0300

    add test1

--- 7.py
+++ 7.py
@@ -1,8 +1,9 @@
 x_boom = 7
-for i in range(1,100):
+for i in range(1, 100):
     if i % x_boom == 0 or str(x_boom) in str(i):
         continue
     print(i)
 
 else:
     print("loop finished successfuly")
+    # my comment
